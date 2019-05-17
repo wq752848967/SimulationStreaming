00f0d23916dc37a39746b64c4e8c61af19ba1645
wq752848967 <752848967@qq.com>
2019-05-17 21:03:11 +0800

    wq: big data test

--- src/main/java/ts/sstreaming/test/Test.java
+++ src/main/java/ts/sstreaming/test/Test.java
@@ -1,7 +1,16 @@
 package ts.sstreaming.test;
 
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SaveMode;
+import org.apache.spark.sql.SparkSession;
+
 public class Test {
     public static void main(String[] args) {
-        System.out.println("hello world!");
+        String masterUrl = "spark://192.168.10.12:7077";
+        SparkSession session = SparkSession.builder().master(masterUrl).getOrCreate();
+        Dataset<Row> ds = session.read().option("header","true").csv("hdfs://192.168.10.12:9000/flok/sim_data_id.csv");
+        Dataset<Row> new_ds = ds.limit(3661658);
+        new_ds.write().option("header","true").mode(SaveMode.Overwrite).csv("hdfs://192.168.10.12:9000/flok/sim_data_small.csv");
     }
 }
