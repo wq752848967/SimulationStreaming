9118743a80989f3b6e74315fe22c423fc01f6f7a
WangQi <wangqi.frank@bytedance.com>
2019-06-15 16:07:00 +0800

    add:sparkjobsubmiter

--- src/main/java/ts/hg_sstreaming/test/Iso_JavaOp.java
+++ src/main/java/ts/hg_sstreaming/test/Iso_JavaOp.java
@@ -6,14 +6,20 @@ import org.apache.spark.sql.SaveMode;
 import org.apache.spark.sql.SparkSession;
 
 public class Iso_JavaOp {
-    String delimiter = "|";
+    private static String delimiter = "|";
     private static String masterUrl = "local[5]";
     //"/Users/wangqi/Desktop/FloK/sim/ios/left_data.csv";//
     private static String left_input = "hdfs://192.168.35.55:9000/flok/4665/DWFReadMySQL-1530083324_475428cc-5f2c-4291-915b-e5c3759b9405_0.output";
     //private static String right_input = "/Users/wangqi/Desktop/FloK/sim/ios/right_data.csv";
     private static String right_input = "hdfs://192.168.35.55:9000/flok/4665/csv_loader-1530083012_dafde4f6-9eda-404a-87df-c2fc51bf0dab_0.output";
     public static void main(String[] args) {
-
+        SparkSession session = SparkSession.builder().master(masterUrl).getOrCreate();
+        Dataset<Row> ds_left = session.read().option("header","true").option("delimiter",delimiter).csv(left_input);
+        Row[] rs_left = (Row[])ds_left.collect();
+        for(Row r:rs_left){
+            String deviceId = "1001"+r.getAs("device_id").toString();
+            System.out.println(deviceId);
+        }
     }
     public void run(){
         SparkSession session = SparkSession.builder().master(masterUrl).getOrCreate();
@@ -21,6 +27,7 @@ public class Iso_JavaOp {
         Dataset<Row> ds_right = session.read().option("header","true").option("delimiter",delimiter).csv(right_input);
         Row[] rs_left = (Row[])ds_left.collect();
         Row[] rs_right =(Row[]) ds_right.collect();
+        ds_left.show();
         for(Row r:rs_left){
             String deviceId = "1001"+r.getAs("device_id").toString();
             String period_end = r.getAs("period_end").toString();
